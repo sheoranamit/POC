2428f2bdb0d367b2f71be1c7872108a590774d33
sheor <sheorana065@gmail.com>
2023-11-30 19:07:25 +0530

    new changes

--- E-commerce/server/index.js
+++ E-commerce/server/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
+const dotenv = require('dotenv');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 
 app.use(cors());
+dotenv.config();
 
 app.get('/api/products', async (req, res) => {
   try {
@@ -30,6 +32,16 @@ app.get('/api/products/:id', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+
+
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
