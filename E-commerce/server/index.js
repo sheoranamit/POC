0f64e9cc5baa8aaa9c00327efcd4a5fff582fd90
sheor <sheorana065@gmail.com>
2023-11-30 19:24:06 +0530

    path

--- E-commerce/server/index.js
+++ E-commerce/server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const dotenv = require('dotenv');
-
+const path =  require ('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
